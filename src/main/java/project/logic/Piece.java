459f925f0f21dd2cb7c68f93e71999aabda24832
juanyachino <juanyachino@gmail.com>
2020-12-31 18:59:08 -0300

    added a getter method for colour

--- src/main/java/project/logic/Piece.java
+++ src/main/java/project/logic/Piece.java
@@ -4,8 +4,12 @@ public class Piece {
     enum Colour {
         WHITE,BLACK
     }
+
     private  Colour colour;
     public void Piece(Colour colour) {
         this.colour = colour;
+    }
+    public Colour getColour() {
+        return colour;
     }
 }
