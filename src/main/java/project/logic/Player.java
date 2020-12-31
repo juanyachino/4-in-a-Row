c37e17d053d8b677a27d3c702e5a3908ef0afe29
juanyachino <juanyachino@gmail.com>
2020-12-31 18:59:54 -0300

    added getter methods

--- src/main/java/project/logic/Player.java
+++ src/main/java/project/logic/Player.java
@@ -1,11 +1,19 @@
 package project.logic;
 
 public class Player {
+
     private Piece.Colour colour;
     private String displayName;
 
     public Player(Piece.Colour colour, String displayName){
         this.colour = colour;
         this.displayName = displayName;
+    }
+    public Piece.Colour getColour() {
+        return colour;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 }
