3d66505e66556852fd0b1587a1e616168e1b84ed
juanyachino <juanyachino@gmail.com>
2021-01-01 23:54:07 -0300

    added new method to insert a piece

--- src/main/java/project/logic/Game.java
+++ src/main/java/project/logic/Game.java
@@ -23,5 +23,23 @@ public class Game {
         players.add(nextPlayer);
     }
 
-
+    /**
+     * inserts a piece in the given column chosen by the given player.
+     * @param column the given column number
+     * @param player the given player
+     * @throws Exception if the turn isn't correct or the column is full.
+     * @return the position of the new inserted piece.
+     */
+    public static int insertPiece(int column, Player player) throws Exception {
+        //check if the turn is correct
+        if(!(player == players.peek())){
+            throw new Exception("this isn't your turn!");
+        }
+        // call board's method insert
+        if(!board.canInsertInto(column)){
+            throw new Exception("the column full!");
+        }
+        Piece thePiece = new Piece(player.getColour());
+        return board.insertInto(column,thePiece);
+    }
 }
