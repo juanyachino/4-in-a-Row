4c9ae9bc778a2ff4ab55838fe73721c63058b271
juanyachino <juanyachino@gmail.com>
2021-01-01 22:52:54 -0300

    added methods regarding player turns

--- src/main/java/project/logic/Game.java
+++ src/main/java/project/logic/Game.java
@@ -1,15 +1,27 @@
 package project.logic;
 
 
+import java.util.Queue;
+
 public class Game {
+
+    private static Queue<Player> players;
     private static Board board;
-    private static Player player1;
-    private static Player player2;
     public Game(){
         board = new Board();
     }
     public static void addPlayers(String displayName1, String displayName2) {
-        player1 = new Player(Piece.Colour.BLACK,displayName1);
-        player1 = new Player(Piece.Colour.WHITE,displayName2);
+        players.add(new Player(Piece.Colour.BLACK,displayName1));
+        players.add(new Player(Piece.Colour.WHITE,displayName2));
     }
+
+    /**
+     * changes the current player's turn.
+     */
+    public static void changeTurn(){
+        Player nextPlayer = players.poll();
+        players.add(nextPlayer);
+    }
+
+
 }
