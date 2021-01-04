507bd135d048f68e4f9886f309d3282b8c89963c
juanyachino <juanyachino@gmail.com>
2021-01-03 21:08:47 -0300

    added endpoints to App.java

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -1,9 +1,41 @@
 package project;
 
+import com.coriant.sdn.ss.SparkSwagger;
+import org.javalite.activejdbc.Base;
+import project.Endpoints.GameEndpoint;
+import project.Endpoints.UserEndpoint;
+import spark.Service;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) {
+    /**
+     * Number of port.
+     */
+    private static final int PORT = 55555;
+
+    /**
+     * Spark Service.
+     */
+    private static Service spark = Service.ignite().port(PORT);
+
+    public static void main(String[] args) throws IOException {
         get("/hello", (req, res) -> "Hello World");
+        SparkSwagger.of(spark)
+                .before(
+                        (request, response) -> {
+                            Base.open();
+                        })
+                .after((request, response) -> {
+                            Base.close();
+                })
+                .endpoints(() -> Arrays.asList(
+                        new UserEndpoint(),
+                        new GameEndpoint()))
+                .generateDoc();
     }
 }
