22077ed99d65bc14c403f795643e22f272d31218
juanyachino <juanyachino@gmail.com>
2021-06-29 16:58:10 -0300

    new test class for Game methods

---
+++ src/test/java/project/logic/GameTest.java
@@ -0,0 +1,43 @@
+package project.logic;
+
+import org.javalite.activejdbc.Base;
+import org.javalite.activejdbc.*;
+import org.javalite.activejdbc.LazyList;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import project.DAO.UserDAO;
+import project.models.User;
+
+
+
+
+public class GameTest {
+    @BeforeAll
+    public static void before() {
+        Base.open();
+        Base.openTransaction();
+    }
+    @AfterAll
+    public static void after() {
+        Base.rollbackTransaction();
+        Base.close();
+    }
+    @Test
+    public void createNewGame() {
+
+        UserDAO.createUser("Juan3","1234",
+                "juan3@mail.com","juantateti");
+        UserDAO.createUser("Juan2","1234",
+                "juan2@mail.com","pepeargento");
+        Game actualGame = new Game("juantateti","pepeargento");
+        assertNotNull(actualGame.getBoard());
+        assertEquals(actualGame.getActualPlayer().getDisplayName(),"juantateti");
+        actualGame.changeTurn();
+        assertEquals(actualGame.getActualPlayer().getDisplayName(),"pepeargento");
+
+    }
+}
